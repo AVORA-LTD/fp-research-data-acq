ac849b68a35cf65747d908a952004bdef3dfd475
Julia <julia.bohutska@avora.com>
2020-12-04 18:32:30 +0000

    first version of the smart alert trigger cronjob

--- cloud-build/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcloud auth activate-service-account ${GOOGLE_APPLICATION_CREDENTIALS} --key-file=/secrets/storage/storage_client.json
-exec "$@"
--- entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcloud auth activate-service-account ${SERVICE_ACCOUNT} --key-file=/secrets/storage/storage_client.json
-exec "$@"
